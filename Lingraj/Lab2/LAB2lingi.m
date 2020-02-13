4bbaf707fc6ec3a45208d34526efc5104ad3e103
abinthomas744 <43981681+abinthomas744@users.noreply.github.com>
2020-02-13 23:32:29 +0530

    Lingraj

---
+++ Lingraj/Lab2/LAB2lingi.m
@@ -0,0 +1,25 @@
+
+%Some basic intensity transformation methods
+a=imread('grayscale.jpg');
+info=imfinfo('grayscale.jpg');
+B=info.BitDepth;
+
+%only use a grayscale image because of how the next line is implemented
+L=2^B;
+s=L-a;
+figure,subplot(1,2,1) 
+imshow(a);
+subplot(1,2,2) 
+imshow(s);
+imwrite(a,'Original.jpg');
+imwrite(s,'Negative.jpg');
+
+
+m=double(a);
+A=.0*log(1+m);
+figure,subplot(1,2,1) 
+imshow(a);
+subplot(1,2,2) 
+imshow(A);
+imwrite(a,'Original1.jpg');
+imwrite(A,'Log_Transform.jpg');
