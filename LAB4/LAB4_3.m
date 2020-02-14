07212700ced4ceb5a09ab0bbb015c10c53960ae0
abinthomas744 <43981681+abinthomas744@users.noreply.github.com>
2020-02-14 23:23:48 +0530

    tifl

--- LAB4/LAB4_3.m
+++ LAB4/LAB4_3.m
@@ -2,15 +2,17 @@ i=imread('grayscale.jpg');
 info=imfinfo('grayscale.jpg');
 w=info.Width;
 h=info.Height;
+b=info.BitDepth;
+p=((2^b)-1);
 
-p=255;
-
-for a=0:w
-    for b=0:h
-        for c=0:p
-            if i(a,b)==p
-            intensity(p)=intensity(p)+1;
+for a=(0:h)
+    for b=(0:w)
+        for c=(0:p)
+            if i(a,b)==c
+            i(p)=i(p)+1;
             end
         end
     end
- end
+end
+
+          
