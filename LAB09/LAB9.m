90cb97f8300da7e44d9c9e55df48b174ae22cfaf
abinthomas744 <43981681+abinthomas744@users.noreply.github.com>
2020-03-09 11:31:34 +0530

    Stage 1

--- LAB09/LAB9.m
+++ LAB09/LAB9.m
@@ -1,10 +1,18 @@
 i=imread('gs_input.jpg');
-h=fspecial('disk',3);
+h=fspecial('disk',5);
+
 blurred=imfilter(i,h,'replicate');
 s=i-blurred;
 s=s*0.3;
 deblurred=i+s;
 
 fig1=figure;imshow(i);
+title('Original');
 fig2=figure;imshow(blurred);
-fig3=figure;imshow(s);
+title('Blurred');
+fig3=figure;imshow(deblurred);
+title('Deblurred');
+
+saveas(fig1,'Original.jpg');
+saveas(fig2,'Blurred.jpg');
+saveas(fig3,'Deblurred.jpg');
